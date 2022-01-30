77919d11057d3a9f61c73bb732b694623b73557b
RNO <arnaud.juracek@gmail.com>
2022-01-30 17:53:27 +0100

    Reinforce bottom bar layout

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,12 @@
 const filter = document.querySelector('input[type=search].filter')
+const filterable = document.querySelector('[data-filterable]')
+const items = filterable && filterable.querySelectorAll('[data-filter]')
+const showAll = document.getElementById('showAll')
 
 if (filter) {
-  const items = document.querySelectorAll('[data-filter]')
-  const showAll = document.getElementById('showAll')
   filter.addEventListener('input', e => {
     if (showAll) showAll.checked = !!filter.value
+    filterable.classList.toggle('is-filtered', !!filter.value)
 
     for (const item of items) {
       const match = item.dataset.filter.toUpperCase().includes(filter.value.toUpperCase())
