7a904a24f0822ed9725ce0e974260192edaf1bcb
Arnaud Juracek <arnaud.juracek@gmail.com>
2022-05-20 11:43:48 +0200

    Always allow API calls if API_TOKEN is explicitly set to false

--- server/api/authenticate.js
+++ server/api/authenticate.js
@@ -2,6 +2,9 @@ module.exports = (req, res, next) => {
   // Allow API calls from authenticated clients
   if (req.session.authenticated) return next()
 
+  // Allow API calls if API_TOKEN is explicitly set to false
+  if (process.env.API_TOKEN === 'false') return next()
+
   // Reject API calls if incorrect API token
   if (req.get('Authorization') !== ('Basic ' + process.env.API_TOKEN)) {
     return res.status(401).json({ error: 'Invalid token' })
