c3575f7a69af1b00c26cce3166e0f0e15661300e
Arnaud Juracek <arnaud.juracek@gmail.com>
2023-04-05 13:17:44 +0200

    Handle non 200 response on bookmarks/add

--- server/api/bookmark/add.js
+++ server/api/bookmark/add.js
@@ -3,27 +3,28 @@ const fetch = require('node-fetch')
 
 module.exports = async (req, res, next) => {
   try {
-    // Ensure webpage exists
-    const response = await fetch(req.body.url)
-
-    // TODO: handle 403 forbidden
-    if (response.status !== 200) {
-      throw new Error(`[${response.status}] ${response.statusText}`)
-    }
-
     // Ensure webpage is not already bookmarked
     const bookmarks = await fs.readJson(process.env.BOOKMARKS)
     for (const index in bookmarks) {
       if (bookmarks[index].url === req.body.url) delete bookmarks[index]
     }
 
+    // Ensure webpage exists
+    const response = await fetch(req.body.url)
+
     // Create a new bookmark
-    const body = await response.text()
-    const title = (body.match(/<title.*?>([\S\s]*?)<\/title>/i) || [])[1]
     const bookmark = {
       timestamp: Date.now(),
-      title: (title || req.body.url).replace(/[\n\r\t]/g, ''),
+      // Use url origin as default
+      title: (req.body.url.match(/https?:\/\/(?:www\.)?([^/]*)/) || [])[1] || req.body.url,
       url: req.body.url
+    }
+
+    // Get bookmark title if possible
+    if (response.status === 200) {
+      const body = await response.text()
+      const title = (body.match(/<title.*?>([\S\s]*?)<\/title>/i) || [])[1]
+      bookmark.title = (title || req.body.url).replace(/[\n\r\t]/g, '')
     }
 
     // Append bookmark to DB
